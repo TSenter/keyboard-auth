91d9153782b84ddd1168e49361ad524d97ec79cc
Tyler Senter <tyler.senter.427@gmail.com>
2021-12-06 06:45:34 -0500

    chore: Move index.js into src folder

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
 app.get('/', (req, res) => {
   console.log(`GET: ${req.url}`);
 
   res.sendFile(`${process.cwd()}/assets/index.html`);
+});
+
+app.post('/api/key-metrics', (req, res) => {
+  const reqData = req.body;
+
+  res.status(202).send();
 });
 
 app.use(express.static(`${process.cwd()}/assets`));
